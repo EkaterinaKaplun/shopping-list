ca97a195378954f06949d1b2800158c24e83cacb
EkaterinaKaplun <Ekaterina.Kaplun.1994@mail.ru>
2024-04-06 09:07:58 +0300

    Создала мини-проект "Список покупок"

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
-const groceries = document.querySelector('.groceries');
-const inputWrapper = document.querySelector('.input-wrapper i');
 const items = document.querySelector('#items');
 const input = document.querySelector('#input');
 
-inputWrapper.addEventListener('click', function() {
-    items.textContent = '';
-})
-
 input.addEventListener('keydown', function(event) {
-    if (event.key == 'Enter') {}
+    if (event.key == 'Enter') {
+        const inputText = input.value;
+
+        const newItem = document.createElement('div');
+        newItem.textContent = inputText;
+
+        newItem.addEventListener('click', function() {
+            newItem.classList.toggle('done');
+        })
+
+        if (inputText != '') {
+            items.append(newItem);
+        }
+
+        input.value = '';
+    }
 })
 
-
-const h2 = document.createElement('h2');
-h2.textContent = input.value;
-
-h2.addEventListener('click', function() {
-    h2.classList.toggle('done');
-})
-
-input.value = '';
 
 
 /* Пустые элементы не должны добавляться */
